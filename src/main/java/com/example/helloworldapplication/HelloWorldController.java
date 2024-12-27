26c30cebdf4102e8228627ddd4eaa9e3336e3a55
PrasadDasari <dvsavprasad@gmail.com>
2024-12-27 21:40:10 +0530

    Updated controller to simplify things to avoid endpoints naming confusion

--- src/main/java/com/example/helloworldapplication/HelloWorldController.java
+++ src/main/java/com/example/helloworldapplication/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.example.helloworldapplication;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.health.Health;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +20,6 @@ public class HelloWorldController {
     @PostConstruct
     public void init() {
         logger.info("Initializing HelloWorldController....");
-        logger.info("IMAGE_TAG environment variable: {}", System.getenv("IMAGE_TAG"));
         String appVersion = System.getenv("IMAGE_TAG");
         logger.info("Currently deployed version for Hello World API: {}", appVersion);
     }
@@ -30,14 +31,23 @@ public class HelloWorldController {
         return "Hello, World from Harness Integration version " + appVersion + "!";
     }
 
-    @GetMapping("/actuator/health")
-    public Health health() {
+    @GetMapping("/health/heartbeat")
+    public ResponseEntity<String> healthCheck() {
+        // Get the app version from the environment variable
         String appVersion = System.getenv("IMAGE_TAG");
+
+        // Log the app version to help with troubleshooting
+        logger.info("Current app version: {}", appVersion);
+
+        // If the app version is v2, return a failure (503 Service Unavailable)
         if ("v2".equals(appVersion)) {
-            // Simulate failure for v2 by returning a DOWN status
-            return Health.down().withDetail("Version", "v2 failed").build();
+            logger.error("Service is unavailable. Version: v2");
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service unavailable for Version " + appVersion);
+        } else {
+            // If the app version is v1, return a success (200 OK)
+            logger.info("Service is up. Version: {}", appVersion);
+            return ResponseEntity.ok("Service is up");
         }
-        return Health.up().build();
     }
 
 }
